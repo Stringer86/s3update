cc5a1a512d229ddedd8a554a693eaf25eab408da
Stephen Stringer <stephenstringer@Stephens-MacBook-Pro.local>
2018-11-28 10:45:27 -0800

    functionality with public s3

--- s3update.go
+++ s3update.go
@@ -3,6 +3,7 @@ package s3update
 import (
 	"fmt"
 	"io/ioutil"
+	"net/http"
 	"os"
 	"os/exec"
 	"runtime"
@@ -11,26 +12,17 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/mitchellh/ioprogress"
 )
 
 type Updater struct {
 	// CurrentVersion represents the current binary version.
 	// This is generally set at the compilation time with -ldflags "-X main.Version=42"
-	// See the README for additional information
 	CurrentVersion string
-	// S3Bucket represents the S3 bucket containing the different files used by s3update.
-	S3Bucket string
-	// S3Region represents the S3 region you want to work in.
-	S3Region string
-	// S3ReleaseKey represents the raw key on S3 to download new versions.
-	// The value can be something like `cli/releases/cli-{{OS}}-{{ARCH}}`
-	S3ReleaseKey string
-	// S3VersionKey represents the key on S3 to download the current version
-	S3VersionKey string
+
+	S3ReleaseUrl string
+
+	S3VersionUrl string
 }
 
 // validate ensures every required fields is correctly set. Otherwise and error is returned.
@@ -39,20 +31,12 @@ func (u Updater) validate() error {
 		return fmt.Errorf("no version set")
 	}
 
-	if u.S3Bucket == "" {
-		return fmt.Errorf("no bucket set")
+	if u.S3ReleaseUrl == "" {
+		return fmt.Errorf("no s3ReleaseUrl set")
 	}
 
-	if u.S3Region == "" {
-		return fmt.Errorf("no s3 region")
-	}
-
-	if u.S3ReleaseKey == "" {
-		return fmt.Errorf("no s3ReleaseKey set")
-	}
-
-	if u.S3VersionKey == "" {
-		return fmt.Errorf("no s3VersionKey set")
+	if u.S3VersionUrl == "" {
+		return fmt.Errorf("no s3VersionUrl set")
 	}
 
 	return nil
@@ -76,7 +60,7 @@ func AutoUpdate(u Updater) error {
 }
 
 // generateS3ReleaseKey dynamically builds the S3 key depending on the os and architecture.
-func generateS3ReleaseKey(path string) string {
+func generateS3ReleaseUrl(path string) string {
 	path = strings.Replace(path, "{{OS}}", runtime.GOOS, -1)
 	path = strings.Replace(path, "{{ARCH}}", runtime.GOARCH, -1)
 
@@ -89,8 +73,8 @@ func runAutoUpdate(u Updater) error {
 		return fmt.Errorf("invalid local version")
 	}
 
-	svc := s3.New(session.New(), &aws.Config{Region: aws.String(u.S3Region)})
-	resp, err := svc.GetObject(&s3.GetObjectInput{Bucket: aws.String(u.S3Bucket), Key: aws.String(u.S3VersionKey)})
+	resp, err := http.Get(u.S3VersionUrl)
+
 	if err != nil {
 		return err
 	}
@@ -103,21 +87,22 @@ func runAutoUpdate(u Updater) error {
 
 	remoteVersion, err := strconv.ParseInt(string(b), 10, 64)
 	if err != nil || remoteVersion == 0 {
+		fmt.Println(err)
 		return fmt.Errorf("invalid remote version")
 	}
 
 	fmt.Printf("s3update: Local Version %d - Remote Version: %d\n", localVersion, remoteVersion)
 	if localVersion < remoteVersion {
 		fmt.Printf("s3update: version outdated ... \n")
-		s3Key := generateS3ReleaseKey(u.S3ReleaseKey)
-		resp, err := svc.GetObject(&s3.GetObjectInput{Bucket: aws.String(u.S3Bucket), Key: aws.String(s3Key)})
+		s3Url := generateS3ReleaseUrl(u.S3ReleaseUrl)
+		resp, err := http.Get(s3Url)
 		if err != nil {
 			return err
 		}
 		defer resp.Body.Close()
 		progressR := &ioprogress.Reader{
 			Reader:       resp.Body,
-			Size:         *resp.ContentLength,
+			Size:         resp.ContentLength,
 			DrawInterval: 500 * time.Millisecond,
 			DrawFunc: ioprogress.DrawTerminalf(os.Stdout, func(progress, total int64) string {
 				bar := ioprogress.DrawTextFormatBar(40)
